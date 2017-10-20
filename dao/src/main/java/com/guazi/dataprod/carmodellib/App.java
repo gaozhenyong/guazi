e0255954189168e104ae5b15351761c3aa003071
gaozhenyong <411468207@qq.com>
2017-10-20 14:33:36 +0800

    first create

---
+++ dao/src/main/java/com/guazi/dataprod/carmodellib/App.java
@@ -0,0 +1,13 @@
+package com.guazi.dataprod.carmodellib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
